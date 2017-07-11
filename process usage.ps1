35f45cb8b665ffb40fbb06a8438e0f7f29d142be
lifeisju <lifeisju@gmail.com>
2017-07-11 10:02:37 +0900

    Sample Process Usage

---
+++ process usage.ps1
@@ -0,0 +1,10 @@
+﻿$CpuCores = (Get-WMIObject Win32_ComputerSystem).NumberOfLogicalProcessors
+$Samples = (Get-Counter "\Process($Processname*)\% Processor Time").CounterSamples
+#$Samples = (Get-Counter "\Processor Information(_Total)\% Processor Time").CounterSamples
+$CPUUsages = $Samples | Select InstanceName, @{Name="CPU %";Expression={[Decimal]::Round(($_.CookedValue / $CpuCores), 1)}}
+
+$List = $CPUUsages | Select-Object -First 5 -Last 1
+
+    if($list[0].InstanceName -eq 'WmiPrvSE' -and [int]$List[0].'CPU %' -ge '99'){
+    Stop-Process -Name 'WmiPrvSE' -Force
+    }
